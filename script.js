df82f7969a3bf37cbdc7a41e8a9e90b3026054aa
NDondero <118403082+NDondero@users.noreply.github.com>
2023-12-05 13:54:18 -0300

    convertion-both-ways

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const convertTemperature = function () {
+const celciusToFarenheit = function () {
     const celcius = document.getElementById("c").value;
     document.getElementById("f").value = (celcius * 9 / 5) + 32;
 }
 
-const convertWeight = function () {
-    const kilograms = document.getElementById("kg").value;
-    document.getElementById("lbs").value = kilograms *2.205;
+const farenheitToCelcius = function () {
+    const farenheit = document.getElementById("f").value;
+    document.getElementById("c").value = (farenheit - 32) * 5 / 9;
 }
 
-const convertDistance = function () {
+const kilogramsToPounds = function () {
+    const kilograms = document.getElementById("kg").value;
+    document.getElementById("lbs").value = kilograms * 2.205;
+}
+
+const poundsToKilograms = function () {
+    const pounds = document.getElementById("lbs").value;
+    document.getElementById("kg").value = pounds / 2.205;
+}
+
+const kilometersToMiles = function () {
     const kilometers = document.getElementById("km").value;
     document.getElementById("m").value = kilometers / 1.609;
+}
+
+const milesToKilometers = function () {
+    const miles = document.getElementById("m").value;
+    document.getElementById("km").value = miles * 1.609;
 }
